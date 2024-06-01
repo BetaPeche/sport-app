bfb6309ef7adf063462428d9e6b80fcec4a6ed3b
BetaPeche <lolopellegrino@gmail.com>
2024-06-01 15:50:06 +0200

    Add condition for change class of root

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,41 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Settings from './pages/Settings'
+
+function RootClassManager() {
+    const location = useLocation()
+
+    useEffect(() => {
+        const root = document.getElementById('root')
+        if (root) {
+            switch (location.pathname) {
+                case '/':
+                    root.className = 'columns-1'
+                    break
+                case '/dashboard':
+                    root.className = 'columns-3'
+                    break
+                default:
+                    root.className = 'columns-3'
+                    break
+            }
+        }
+    }, [location])
+
+    return null
+}
 
 function App() {
     return (
         <BrowserRouter>
+            <RootClassManager />
             <Routes>
                 <Route path="/" element={<Home />} />
-                {/* <Route path="/:id" element={<Project />} />
-                <Route path="*" element={<Home />} /> */}
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/settings" element={<Settings />} />
+                {/* Autres routes ici */}
             </Routes>
         </BrowserRouter>
     )
