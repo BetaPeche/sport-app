b86eac866dda79abdf31798f5e608c8bfe82b596
BetaPeche <lolopellegrino@gmail.com>
2024-06-01 15:50:28 +0200

    Add link

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom'
+
 const Header = () => {
     return (
         <header>
-            <h1>Hello world!</h1>
+            <h1>
+                <Link to={'/'}>My SPORT APP</Link>
+            </h1>
+            <div className="header_user">
+                Loïc <i className="fa-regular fa-face-smile"></i>
+            </div>
         </header>
     )
 }
