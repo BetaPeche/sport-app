8f8e166c55e94498edd845113e2c1c764c3d3a10
BetaPeche <lolopellegrino@gmail.com>
2024-06-02 22:42:44 +0200

    Import 'profil' page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Settings from './pages/Settings'
+import Profil from './pages/Profil'
 
 function RootClassManager() {
     const location = useLocation()
@@ -35,6 +36,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/settings" element={<Settings />} />
+                <Route path="/profil" element={<Profil />} />
                 {/* Autres routes ici */}
             </Routes>
         </BrowserRouter>
