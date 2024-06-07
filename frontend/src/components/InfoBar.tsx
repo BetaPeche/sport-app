6af733f26d4d82714420e06401169c3625e17326
BetaPeche <lolopellegrino@gmail.com>
2024-06-07 12:29:45 +0200

    Creating component logic

--- frontend/src/components/InfoBar.tsx
+++ frontend/src/components/InfoBar.tsx
@@ -1,5 +1,41 @@
-const InfoBar = () => {
-    return <div></div>
+import { useEffect, useState } from 'react'
+
+type InfoBarProps = {
+    name: string
+    value: string | number
+    color: string
+}
+
+const InfoBar: React.FC<InfoBarProps> = ({ name, value, color }) => {
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        const handleLoad = () => {
+            setIsLoaded(true)
+        }
+        window.addEventListener('load', handleLoad)
+
+        return () => {
+            window.removeEventListener('load', handleLoad)
+        }
+    }, [])
+
+    const progressBarStyle = {
+        '--progress-width': isLoaded ? `${value}%` : '0%',
+    } as React.CSSProperties
+
+    return (
+        <div className="statistics__item">
+            <div className="statistics__label">{name}</div>
+            <div className="statistics__progress">
+                <div
+                    className={`statistics__progress-bar statistics__progress-bar--${color}`}
+                    style={progressBarStyle}
+                ></div>
+                <div className="statistics__progress-value">{value}%</div>
+            </div>
+        </div>
+    )
 }
 
 export default InfoBar
