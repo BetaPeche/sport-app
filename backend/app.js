0b1046a3b47b8b1265e310fa3b1b6c75d64739ce
BetaPeche <lolopellegrino@gmail.com>
2024-06-10 17:07:10 +0200

    Remove options from cors

--- backend/app.js
+++ backend/app.js
@@ -3,21 +3,14 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
-const cors = require('cors')
+const cors = require('cors');
 const userRoutes = require('./routes/user')
 
 mongoose.connect(process.env.DB_LINK)
-	.then(() => console.log('Connexion à MongoDB réussie !'))
-	.catch((error) => console.log('Connexion à MongoDB échouée !'))
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch((error) => console.log('Connexion à MongoDB échouée !'))
 
-
-const corsOptions = {
-	origin: ['https://sport-app-eosin.vercel.app', 'https://sport-app-gx5q.vercel.app'],
-	methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-	allowedHeaders: 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-}
-	
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json())
 
