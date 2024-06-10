4bcf65123f49fdb22d41e41adc842606850a7a57
BetaPeche <lolopellegrino@gmail.com>
2024-06-10 17:00:40 +0200

    Use cors

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 const userRoutes = require('./routes/user')
 
 mongoose.connect(process.env.DB_LINK)
@@ -10,12 +11,13 @@ mongoose.connect(process.env.DB_LINK)
 	.catch((error) => console.log('Connexion à MongoDB échouée !'))
 
 
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-	next();
-})
+const corsOptions = {
+	origin: ['https://sport-app-eosin.vercel.app', 'https://sport-app-gx5q.vercel.app'],
+	methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+	allowedHeaders: 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+}
+	
+app.use(cors(corsOptions));
 
 app.use(express.json())
 
