35f513d5616a5aa8668053faf16db3e3cd46a682
BetaPeche <lolopellegrino@gmail.com>
2024-06-10 16:20:34 +0200

    Replace div by span

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps> = ({ text, color, action }) => {
             {text}
             {color && (
                 <div className="hoverEffect">
-                    <div></div>
+                    <span></span>
                 </div>
             )}
         </button>
