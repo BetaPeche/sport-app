970468c8cc70f95b21f1e556fce1e80aaae2b56f
BetaPeche <lolopellegrino@gmail.com>
2024-06-11 17:01:11 +0200

    Add token verification

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import Button from '../components/Button'
+import { useEffect, useState } from 'react'
 
 const root: HTMLElement | null = document.getElementById('root')
 if (root) {
@@ -7,17 +8,60 @@ if (root) {
 }
 
 const Home = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        const validateToken = async () => {
+            try {
+                const response = await fetch(
+                    `${import.meta.env.VITE_URL_API}/api/auth/token`,
+                    {
+                        method: 'POST',
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem(
+                                'token'
+                            )}`,
+                        },
+                    }
+                )
+                if (response.ok) {
+                    setIsAuthenticated(true)
+                }
+            } catch (error) {
+                console.error('Error validating token:', error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        if (localStorage.getItem('token')) {
+            validateToken()
+        } else {
+            setIsLoading(false)
+            setIsAuthenticated(false)
+        }
+    }, [])
+
+    if (isLoading) {
+        return
+    }
+
     return (
         <main className="home">
             <h1>Outil de suivi du poids et de la composition corporelle</h1>
-            <div className="main__buttons">
-                <Link to="/signup">
-                    <Button text="Inscription" color={false} />
+            {isAuthenticated ? (
+                <Link to="/dashboard">
+                    <Button text="Mon Dashboard" color={true} />
                 </Link>
-                <Link to="/login">
-                    <Button text="Connexion" color={true} />
-                </Link>
-            </div>
+            ) : (
+                <div className="main__buttons">
+                    <Link to="/signup">
+                        <Button text="Inscription" color={false} />
+                    </Link>
+                    <Link to="/login">
+                        <Button text="Connexion" color={true} />
+                    </Link>
+                </div>
+            )}
         </main>
     )
 }
