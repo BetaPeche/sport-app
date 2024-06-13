b81eb62811c4b81f3ecae6b48ef763141a00de34
BetaPeche <lolopellegrino@gmail.com>
2024-06-13 08:36:45 +0200

    Data hours format

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -14,18 +14,18 @@ import Navigation from '../components/Navigation'
 import InfoBar from '../components/InfoBar'
 
 const data = [
-    { date: '01/03/24', poids: 80 },
-    { date: '01/03/24', poids: 78 },
-    { date: '01/03/24', poids: 90.5 },
-    { date: '01/03/24', poids: 75 },
-    { date: '01/03/24', poids: 81 },
-    { date: '01/03/24', poids: 78 },
-    { date: '01/03/24', poids: 75 },
-    { date: '01/03/24', poids: 80 },
-    { date: '01/03/24', poids: 88 },
-    { date: '01/03/24', poids: 83 },
-    { date: '01/03/24', poids: 82 },
+    { date: new Date('2021-05-30T00:00:00Z'), poids: 91 },
+    { date: new Date('2021-06-06T00:00:00Z'), poids: 93 },
+    { date: new Date('2021-06-13T00:00:00Z'), poids: 91.15 },
+    { date: new Date('2021-06-15T00:00:00Z'), poids: 90.95 },
+    { date: new Date('2021-06-20T00:00:00Z'), poids: 90.45 },
+    { date: new Date('2021-06-27T00:00:00Z'), poids: 82.4 },
 ]
+
+const parsedData = data.map((item) => ({
+    ...item,
+    date: item.date.getTime(),
+}))
 
 const minWeight = Math.min(...data.map((item) => item.poids))
 const maxWeight = Math.max(...data.map((item) => item.poids))
@@ -41,17 +41,17 @@ function roundToNearestFiveDown(num: number): number {
 let roundedMinWeight = roundToNearestFiveDown(minWeight)
 let roundedMaxWeight = roundToNearestFiveUp(maxWeight)
 
-if (roundedMinWeight === minWeight) {
+if (roundedMinWeight - minWeight > -2.5) {
     roundedMinWeight -= 5
 }
-if (roundedMaxWeight === maxWeight) {
+if (roundedMaxWeight - maxWeight < 2.5) {
     roundedMaxWeight += 5
 }
 
 const renderLineChart = (
     <ResponsiveContainer width="100%" height={380}>
         <AreaChart
-            data={data}
+            data={parsedData}
             margin={{ top: 20, right: 26, left: 0, bottom: 0 }}
         >
             <defs>
@@ -64,8 +64,18 @@ const renderLineChart = (
                 tickMargin={10}
                 stroke={'#fff'}
                 dataKey="date"
+                type="number"
+                domain={['dataMin', 'dataMax']}
+                tickFormatter={(tick) =>
+                    new Date(tick).toLocaleDateString('fr-FR', {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: '2-digit',
+                    })
+                }
+                scale="time"
                 axisLine={false}
-                interval={1}
+                interval="preserveStartEnd"
                 fontSize={12}
             />
             <YAxis
@@ -87,21 +97,8 @@ const renderLineChart = (
                 }}
                 itemStyle={{ color: '#fff' }}
                 formatter={(value) => [`${value} Kg`, 'Poids']}
+                labelFormatter={(label) => new Date(label).toLocaleDateString()}
             />
-            <ReferenceLine
-                y={80}
-                ifOverflow="extendDomain"
-                stroke="red"
-                strokeWidth={2}
-            >
-                <Label
-                    value="Objectif"
-                    position="top"
-                    fill="white"
-                    fontSize={12}
-                />
-            </ReferenceLine>
-
             <Area
                 type="monotone"
                 dataKey="poids"
@@ -110,11 +107,28 @@ const renderLineChart = (
                 fill="url(#gradient)"
                 animationDuration={1000}
             />
+            <ReferenceLine
+                y={78}
+                ifOverflow="hidden"
+                stroke="red"
+                strokeWidth={2}
+            >
+                <Label
+                    value="Objectif"
+                    position="insideBottomLeft"
+                    fill="white"
+                    fontSize={14}
+                    offset={10}
+                    style={{
+                        textShadow: '1px 1px 3px #000',
+                        fontWeight: 500,
+                    }}
+                />
+            </ReferenceLine>
         </AreaChart>
     </ResponsiveContainer>
 )
 const Dashboard = () => {
-    // console.log(import.meta.env.VITE_URL_API)
     return (
         <>
             <Header />
