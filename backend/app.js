6480126a5ff1531ca84e71e12174d57535b47e70
BetaPeche <lolopellegrino@gmail.com>
2024-06-14 14:27:11 +0200

    Add PUT method and import 'user' routes

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,34 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
-const cors = require('cors');
+const cors = require('cors')
 const userRoutes = require('./routes/user')
 const authRoutes = require('./routes/auth')
+const userDataRoutes = require('./routes/userData')
 
-mongoose.connect(process.env.DB_LINK)
+mongoose
+    .connect(process.env.DB_LINK)
     .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch((error) => console.log('Connexion à MongoDB échouée !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
 
-	app.use(cors(
-		{
-			origin: ["https://sport-app-eosin.vercel.app","http://localhost:5173"],
-			methods: ["POST", "GET"],
-			credentials: true
-		}
-	));
+app.use(
+    cors({
+        origin: ['https://sport-app-eosin.vercel.app', 'http://localhost:5173'],
+        methods: ['POST', 'GET', 'PUT'],
+        credentials: true,
+    })
+)
 
 app.use(express.json())
 
 app.use('/api/auth', userRoutes)
 app.use('/api/auth', authRoutes)
+app.use('/user', userDataRoutes)
 
-app.get("/", (req, res) => {
-	res.json("Ready to go !");
-}) 
+app.get('/', (req, res) => {
+    res.json('Ready to go !')
+})
 
 module.exports = app
