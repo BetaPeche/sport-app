e3204a24b3006eccf65bbe5a57b25f8741d07dbe
BetaPeche <lolopellegrino@gmail.com>
2024-06-17 09:56:56 +0200

    Create schema

---
+++ backend/models/userData.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userDataSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    weight: { type: Number },
+    muscularMass: { type: Number },
+    water: { type: Number },
+    visceralFat: { type: Number },
+    protein: { type: Number },
+})
+
+module.exports = mongoose.model('UserData', userDataSchema)
