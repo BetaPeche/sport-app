86b2249a5f0271af5d617d5a33f04330fe24b9e3
BetaPeche <lolopellegrino@gmail.com>
2024-06-20 16:25:59 +0200

    Delete validator

--- backend/models/userProfile.js
+++ backend/models/userProfile.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
 
 const userProfileSchema = mongoose.Schema({
-	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-	activity: { type: Number, required: true },
-	age: { type: Number, required: true },
-	gender: { type: Number, required: true },
-	height: { type: Number, required: true },
-	name: { type: String, required: true },
-	objectiveWeight: { type: Number, required: true },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    activity: { type: Number, required: true },
+    age: { type: Number, required: true },
+    gender: { type: Number, required: true },
+    height: { type: Number, required: true },
+    name: { type: String, required: true },
+    objectiveWeight: { type: Number, required: true },
 })
 
 module.exports = mongoose.model('UserProfile', userProfileSchema)
