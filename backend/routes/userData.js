3e3d64f95cfd520f9a673e8e9231633b89583d63
BetaPeche <lolopellegrino@gmail.com>
2024-06-20 16:26:22 +0200

    Add routes for data

--- backend/routes/userData.js
+++ backend/routes/userData.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const userProfileCtrl = require('../controllers/userProfile')
+const userDataCtrl = require('../controllers/userData')
 const auth = require('../middleware/auth')
 
 router.post('/profil', userProfileCtrl.postUserProfile)
 router.get('/profil/:userId', auth, userProfileCtrl.getUserProfile)
 router.put('/profil/:userId', auth, userProfileCtrl.updateUserProfile)
+
+router.post('/data', userDataCtrl.postUserData)
+router.get('/data/:userId', auth, userDataCtrl.getUserData)
 
 module.exports = router
