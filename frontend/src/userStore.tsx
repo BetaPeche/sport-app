7801c9e1036d9cb8f71fe1938b8cfbe2b147467b
BetaPeche <lolopellegrino@gmail.com>
2024-06-21 12:26:01 +0200

    Create store

---
+++ frontend/src/userStore.tsx
@@ -0,0 +1,41 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+type DataItem = {
+    _id?: string
+    userId: string
+    date: Date
+    weight: number | null
+    muscularMass: number | null
+    water: number | null
+    visceralFat: number | null
+    protein: number | null
+    __v?: number
+}
+
+type UserDataStore = {
+    data: DataItem[]
+    addData: (newData: DataItem) => void
+    setData: (data: DataItem[]) => void
+}
+
+const useUserDataStore = create<UserDataStore>()(
+    persist(
+        (set) => ({
+            data: [],
+            addData: (newData: DataItem) =>
+                set((state) => ({
+                    data: [...state.data, newData],
+                })),
+            setData: (data: DataItem[]) =>
+                set(() => ({
+                    data,
+                })),
+        }),
+        {
+            name: 'user-data-storage',
+        }
+    )
+)
+
+export default useUserDataStore
