7fbfaf86237ef278e35204c482ddc10d19176652
BetaPeche <lolopellegrino@gmail.com>
2024-06-26 09:34:17 +0200

    Import multer

--- backend/routes/userData.js
+++ backend/routes/userData.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const userProfileCtrl = require('../controllers/userProfile')
 const userDataCtrl = require('../controllers/userData')
 const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
 
-router.post('/profil', userProfileCtrl.postUserProfile)
+router.post('/profil', multer, userProfileCtrl.postUserProfile)
 router.get('/profil/:userId', auth, userProfileCtrl.getUserProfile)
-router.put('/profil/:userId', auth, userProfileCtrl.updateUserProfile)
+router.put('/profil/:userId', auth, multer, userProfileCtrl.updateUserProfile)
 
 router.post('/data', userDataCtrl.postUserData)
 router.get('/data/:userId', auth, userDataCtrl.getUserData)
