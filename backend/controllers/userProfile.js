b8cb0263127d892edb27e226ac441447d3c68571
BetaPeche <lolopellegrino@gmail.com>
2024-06-26 10:34:39 +0200

    Install vercel blob

--- backend/controllers/userProfile.js
+++ backend/controllers/userProfile.js
@@ -1,4 +1,6 @@
 const UserProfile = require('../models/userProfile')
+const { createWriteStream, getURL } = require('@vercel/blob')
+const { v4: uuidv4 } = require('uuid')
 
 exports.postUserProfile = (req, res) => {
     const userprofile = new UserProfile(req.body)
@@ -40,13 +42,28 @@ exports.updateUserProfile = (req, res) => {
     let updateData = { ...req.body }
 
     if (req.file) {
-        const imagePath = `${req.protocol}://${req.get('host')}/images/${
-            req.file.filename
-        }`
-        updateData.imageUrl = imagePath
-    }
+        const fileId = uuidv4()
+        const fileStream = createWriteStream({ path: `/images/${fileId}` })
+        fileStream.end(req.file.buffer, async (err) => {
+            if (err) {
+                return res
+                    .status(500)
+                    .json({ error: 'Erreur lors du téléchargement du fichier' })
+            }
+            const imageUrl = await getURL(fileStream.path)
+            updateData.imageUrl = imageUrl
 
-    UserProfile.updateOne({ userId: userId }, updateData)
-        .then(() => res.status(200).json({ message: 'Données mises à jour !' }))
-        .catch((error) => res.status(400).json({ error }))
+            UserProfile.updateOne({ userId: userId }, updateData)
+                .then(() =>
+                    res.status(200).json({ message: 'Données mises à jour !' })
+                )
+                .catch((error) => res.status(400).json({ error }))
+        })
+    } else {
+        UserProfile.updateOne({ userId: userId }, updateData)
+            .then(() =>
+                res.status(200).json({ message: 'Données mises à jour !' })
+            )
+            .catch((error) => res.status(400).json({ error }))
+    }
 }
